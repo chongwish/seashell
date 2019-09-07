d6891033a3c0ccf6495c417399413be4564592f8
chongwish <chongwish@gmail.com>
2019-09-08 00:39:04 +0800

    add appscript

---
+++ swim/steam.sh
@@ -0,0 +1,15 @@
+source $SEASHELL_HOME/seashell.sh
+
+import Linux.Distribution[is]
+import Linux.Container.LXD
+import Linux.Container.LXD.Video
+import Linux.Container.LXD.Audio
+
+export container_name="steam"
+
+is ubuntu
+
+load_host_systemd_pulseaudio $container_name
+load_host_xorg $container_name
+
+exec_container_command `whoami`@$container_name "steam"
