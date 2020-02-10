26baa7fa3bcfd60a72bcd2525e84bd8fe19d0576
chongwish <chongwish@gmail.com>
2020-02-10 15:59:31 +0800

    add some application

---
+++ swim/dbeaver.sh
@@ -0,0 +1,19 @@
+source $SEASHELL_HOME/seashell.sh
+
+import Linux.Distribution[is]
+import Linux.Container.LXD
+import Linux.Container.LXD.Video
+import Linux.Container.LXD.Audio
+
+export container_name="archlinux"
+
+is archlinux
+
+load_host_systemd_pulseaudio $container_name
+load_host_xorg $container_name
+
+if [[ -z "`exec_container_command $container_name "which dbeaver"`" ]]; then
+    install_container_package $container_name "dbeaver"
+fi
+
+exec_container_command `whoami`@$container_name "dbeaver"
